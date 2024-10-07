04c25b5fe77c1066cbd15fb09578296b55b5e3a4
DavSan <david.sanchez.martinez@outlook.com>
2024-10-07 10:13:43 +0100

    Update Meme.jsx

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const Meme = () => {
     return (
         <form>
-            <label for="topText">Top text</label>
-            <input type="text" />
-            <label for="bottomText">Bottom text</label>
-            <input type="text" placeholder=''/>
+            <div className='inputs'>
+                <div className='inputs_-top'>
+                    <label for="topText">Top text</label>
+                    <input type="text" />
+                </div>
+                <div className='inputs__bottom'>
+                    <label for="bottomText">Bottom text</label>
+                    <input type="text" placeholder='' />
+                </div>
+            </div>
             <button>Get a new meme image</button>
         </form>
     )
