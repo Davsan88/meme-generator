864f7181663c96f42c446c97b6be69c80a62f40d
DavSan <david.sanchez.martinez@outlook.com>
2024-10-07 12:30:37 +0100

    Update Meme.jsx

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -6,9 +6,15 @@ const Meme = () => {
         <main>
             <form className='form'>
                 <label className='form__label'>
+                    <span>
+                        Top text
+                    </span>
                     <input type="text" placeholder='Top text' className='form__input'/>
                 </label>
                 <label className='form__label'>
+                    <span>
+                        Bottom text
+                    </span>
                     <input type="text" placeholder='Buttom text' className='form__input'/>
                 </label>
                 <button className='form__button'>Get a new meme image 🖼</button>
