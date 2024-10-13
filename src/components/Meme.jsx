e6c278ef660e835c5dcea921c3a85c354d86441d
DavSan <david.sanchez.martinez@outlook.com>
2024-10-13 18:46:56 +0100

    Update Meme.jsx
     Implemented a max-width container to prevent content from becoming too large on bigger screens.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -11,48 +11,50 @@ const Meme = () => {
         // a. Retrieve the array of meme objects from memesData
         const memesArray = memesData.data.memes
         // b. Generate a random index based on the length of the memes array
-        const randomIndex = Math.floor(Math.random() * memesArray.length)  
+        const randomIndex = Math.floor(Math.random() * memesArray.length)
         // c. Get the URL of the randomly selected meme
         const memeUrl = memesArray[randomIndex].url
         // d. Update the state with the new meme image URL
         setMemeImage(memeUrl)
         console.log(memeUrl)
-    } 
+    }
 
     return (
         <main>
-            <div className='form'>
-                <label className='form__label' htmlFor='topText'>
-                    Top text
-                    <input
-                        type="text"
-                        id="topText"
-                        name="topText"
-                        placeholder='Top text'
-                        className='form__input'
-                    />
-                </label>
+            <div className='container'>
+                <div className='form'>
+                    <label className='form__label' htmlFor='topText'>
+                        Top text
+                        <input
+                            type="text"
+                            id="topText"
+                            name="topText"
+                            placeholder='Top text'
+                            className='form__input'
+                        />
+                    </label>
 
-                <label className='form__label' htmlFor='bottomText'>
-                    Bottom text
-                    <input
-                        type="text"
-                        id="bottomText"
-                        name="bottomText"
-                        placeholder='Buttom text'
-                        className='form__input'
-                    />
-                </label>
-                {/* 3. Call the generateMeme function when the button is click */}
-                <button 
-                    className='form__button'
-                    onClick={generateMeme}
-                >
-                    Get a new meme image 🖼
-                </button>
+                    <label className='form__label' htmlFor='bottomText'>
+                        Bottom text
+                        <input
+                            type="text"
+                            id="bottomText"
+                            name="bottomText"
+                            placeholder='Buttom text'
+                            className='form__input'
+                        />
+                    </label>
+                    {/* 3. Call the generateMeme function when the button is click */}
+                    <button
+                        className='form__button'
+                        onClick={generateMeme}
+                    >
+                        Get a new meme image 🖼
+                    </button>
+                </div>
+                {/* 4. Display the meme image using the memeImage state */}
+                <img className='memeImage' src={memeImage} alt='Meme' />
             </div>
-            {/* 4. Display the meme image using the memeImage state */}
-            <img className='memeImage' src={memeImage} alt='Meme' />
         </main>
     )
 }
