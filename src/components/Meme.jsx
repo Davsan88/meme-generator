73a8f2ddce294867533369a5ebce03de7ce9bf20
Dav <david.sanchez.martinez@outlook.com>
2024-12-10 21:55:04 +0000

    Update Meme.jsx

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,5 @@
-import React { useState, useEffect } from 'react'
-import memesData from '../memesData'
+import React, { useState, useEffect } from 'react'
+// import memesData from '../memesData'
 
 
 const Meme = () => {
